1ebfa059cae8cd4de78608556a551fd468714a4c
kris994 <kristipomo@gmail.com>
2020-08-19 14:12:20 +0200

    Created classes based on the diagram

---
+++ Dan_LIV_Kristina_Garcia_Francisco/MotorVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dan_LIV_Kristina_Garcia_Francisco
+{
+    abstract class MotorVehicle
+    {
+        double MotorVolume { get; set; }
+        public int Weight { get; set; }
+        public string Category { get; set; }
+        public string MotorType { get; set; }
+        public string Color { get; set; }
+        public int MotorNumber { get; set; }
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Turn on the motor and start going.");
+        }
+        public virtual void Stop()
+        {
+            Console.WriteLine("Turn off the motor.");
+        }
+    }
+}
---
+++ Dan_LIV_Kristina_Garcia_Francisco/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dan_LIV_Kristina_Garcia_Francisco
+{
+    class Truck : MotorVehicle
+    {
+        public double LoadCapacity { get; set; }
+        public double Hight { get; set; }
+        public int SeatNumber { get; set; }
+
+        public void Load()
+        {
+            Console.WriteLine("Fill up the truck");
+        }
+
+        public void Unload()
+        {
+            Console.WriteLine("Remove items from the truck");
+        }
+    }
+}
